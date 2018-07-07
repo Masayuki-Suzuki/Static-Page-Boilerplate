5b90cf60ab72e00a4603fcb864c76cf8659076b5
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2018-07-07 12:18:32 -0700

    [UpDate] v2.0.1 Prettier Setup. (For PhpStorm / WebStorm. Don't forget add Prettier plugin for your IDE.)

--- assets/js/app.ts
+++ assets/js/app.ts
@@ -1,20 +1,22 @@
 console.log('connected!!')
 
+// Test Code.
 class Human {
-  protected gender :string
-  protected age :number
-  constructor(gender :string, age :number) {
+  protected gender: string
+  protected age: number
+  constructor(gender: string, age: number) {
     this.gender = gender
     this.age = age
   }
   showDetail() {
-    console.log(`You're ${this.age} years old ${this.gender}`);
+    let ary: string[] = []
+    console.log(`You're ${this.age} years old ${this.gender}`)
   }
 }
 
 class Person extends Human {
-  name :string
-  constructor(name :string, gender :string, age :number) {
+  name: string
+  constructor(name: string, gender: string, age: number) {
     super(gender, age)
     this.name = name
   }
